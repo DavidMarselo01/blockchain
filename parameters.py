f8b2d0c082c6b256d74a88e28e4273d4e1a7f16d
DavidMarselo01 <dn1143@nyu.edu>
2020-05-06 03:17:24 -0400

    Delete parameters.py

--- parameters.py
+++
@@ -1,5 +0,0 @@
-#PARAMETERS
-chance_a_validator_is_offline = 5
-chance_a_validator_lies = 15
-chance_a_validator_validates = 100 - (chance_a_validator_lies + chance_a_validator_is_offline)
-transactions_per_block = 5
