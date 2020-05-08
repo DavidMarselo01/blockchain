704c579a155bfc3acd5291e08e47dbdf8d020a55
DavidMarselo01 <dn1143@nyu.edu>
2020-05-07 23:23:26 -0400

    Delete vote.py

--- vote.py
+++
@@ -1,5 +0,0 @@
-class Vote:
-    def __init__(self, currentBlockHash, previousBlockHash, ):
-        self.currentBlockHash = currentBlockHash
-        self.previousBlockHash = previousBlockHash
-        self.user = user
